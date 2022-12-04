52905fe0fabf36aaf908dfb8afa67c6f282effdc
Tomasz Kielar <git@k1eu.dev>
2022-12-04 03:34:22 +0100

    chore: rename components

--- apps/docs/src/config.ts
+++ apps/docs/src/config.ts
@@ -49,9 +49,8 @@ export const SIDEBAR: Sidebar = {
 	en: {
 		'Section Header': [
 			{ text: 'Introduction', link: 'en/introduction' },
-			{ text: 'Page 2', link: 'en/page-2' },
-			{ text: 'Page 3', link: 'en/page-3' },
+			{ text: 'Button', link: 'en/01-button' },
 		],
-		'Another Section': [{ text: 'Page 4', link: 'en/page-4' }],
+		'Another Section': [{ text: 'Text', link: 'en/02-text' }],
 	},
 };
