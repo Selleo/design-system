6cb336441274660456f08115f602db575d0278c6
Michał Staśkiewicz <staskiewicz.miko@gmail.com>
2022-12-16 15:15:46 +0100

    feat: introduce Preview component

--- packages/selleo-design-core/src/Button.tsx
+++ packages/selleo-design-core/src/Button.tsx
@@ -3,3 +3,7 @@ import { h } from "preact";
 export function Button() {
   return <button class="inline-flex bg-orange-500">Button</button>;
 }
+
+export function ButtonExample() {
+  return <button class="inline-flex bg-orange-700">Button</button>;
+}
