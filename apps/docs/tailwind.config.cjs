c056cd342e2ffae616f27a6dbeef0faf46d566df
Michał Staśkiewicz <staskiewicz.miko@gmail.com>
2022-12-16 11:21:05 +0100

    chore: add spacing config

--- apps/docs/tailwind.config.cjs
+++ apps/docs/tailwind.config.cjs
@@ -1,9 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}", "../../packages/selleo-design-core/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  content: [
+    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+    "../../packages/selleo-design-core/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+  ],
   theme: {
+    spacing: {
+      0.5: "4px",
+      1: "8px",
+      2: "16px",
+      3: "24px",
+      4: "32px",
+      5: "40px",
+      6: "72px",
+      7: "96px",
+      8: "144px",
+      9: "192px",
+    },
     extend: {},
   },
   plugins: [],
-	presets: [require('@selleo/tailwind/tailwind.config.cjs')],
+  presets: [require("@selleo/tailwind/tailwind.config.cjs")],
 };
