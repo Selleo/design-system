b544eff71e0f10ebb016f070853dc3d9ff0e083b
matgorn <m.gorny@selleo.com>
2023-01-11 14:07:02 +0100

    chore: add brand color palette to tailwind config

--- packages/selleo-tailwind/tailwind.config.cjs
+++ packages/selleo-tailwind/tailwind.config.cjs
@@ -14,10 +14,41 @@ module.exports = {
       8: "144px",
       9: "192px",
     },
+    colors: {
+      white: '#ffffff',
+      black: '#2F3033',
+      neutral: {
+        100: '#ff6d2a',
+        200: '#eaeaeb',
+        300: '#bfc0c4',
+        400: '#95979d',
+        500: '#696b72',
+        600: '#4c4d52',
+      },
+    },
+    letterSpacing: {
+      normal: '0.01em',
+    },
     extend: {
       colors: {
-				'primary-500': '#FAFAFA',
-			}
+        brand: {
+          primary: {
+            DEFAULT: '#ff6d2a',
+            100: '#fff8f5',
+            200: '#ffeae0',
+            400: '#ff905d',
+            500: '#ff6d2a',
+            600: '#e25718',
+          },
+          secondary: {
+            DEFAULT: '#2aaef4',
+            100: '#f3f7fc',
+            200: '#5ac0f6',
+            500: '#2aaef4',
+            600: '#3871a5'
+          },
+        },
+      }
     },
   },
   plugins: [],
