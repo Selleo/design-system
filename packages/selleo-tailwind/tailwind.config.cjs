04817fec6a5579528f3932e606830cafa29213c0
matgorn <m.gorny@selleo.com>
2023-01-18 15:34:44 +0100

    chore: setup tailwind config based on figma selleo design system

--- packages/selleo-tailwind/tailwind.config.cjs
+++ packages/selleo-tailwind/tailwind.config.cjs
@@ -1,8 +1,26 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: 'class',
   theme: {
+    fontFamily: {
+      sans: ['sofia-pro', ...defaultTheme.fontFamily.sans],
+      'sofia-pro-soft': ['sofia-pro-soft', ...defaultTheme.fontFamily.sans],
+    },
+    fontSize: {
+      xs: '14px',
+      sm: '16px',
+      base: '18px',
+      md: '24px',
+      lg: '32px',
+      xl: '40px',
+      xxl: '56px',
+      xxxl: '72px'
+    },
     spacing: {
+      0: 0,
       0.5: "4px",
       1: "8px",
       2: "16px",
@@ -16,8 +34,44 @@ module.exports = {
     },
     extend: {
       colors: {
-				'primary-500': '#FAFAFA',
-			}
+        brand: {
+          primary: {
+            100: '#fff8f5',
+            200: '#ffeae0',
+            400: '#ff905d',
+            500: '#ff6d2a', //DEFAULT
+            600: '#e25718',
+          },
+          secondary: {
+            100: '#f3f7fc',
+            200: '#5ac0f6',
+            500: '#2aaef4', //DEFAULT
+            600: '#3871a5'
+          },
+        },
+        white: '#ffffff',
+        black: '#2F3033',
+        danger: '#E81B2F',
+        neutral: {
+          100: '#ff6d2a',
+          200: '#eaeaeb',
+          300: '#bfc0c4',
+          400: '#95979d',
+          500: '#696b72',
+          600: '#4c4d52',
+        },
+      },
+      letterSpacing: {
+        normal: '0.01em',
+        wide: '0.15em'
+      },
+      lineHeight: {
+        tight: '21px',
+        relaxed: '1.75'
+      },
+      fontWeight: {
+        black: 800
+      }
     },
   },
   plugins: [],
