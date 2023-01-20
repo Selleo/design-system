147a3ab6a2b5ca4c5cdffa4159a4faf1f49ca223
snacksncode <snacksncode@gmail.com>
2023-01-20 10:00:58 +0100

    feat: add paragraph page

---
+++ packages/selleo-design-core/src/Paragraph.tsx
@@ -0,0 +1,46 @@
+import { h } from "preact";
+
+export function ParagraphBody() {
+  return (
+    <p class="dark:text-white text-base font-sans font-normal leading-[175%] tracking-normal text-neutral-400">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus a enim sit amet sapien laoreet gravida sed
+      lacinia velit. Mauris placerat nisi vel erat suscipit commodo.
+    </p>
+  );
+}
+
+export function ParagraphBodyBold() {
+  return (
+    <p class="dark:text-white text-base font-sans font-bold leading-[175%] tracking-normal text-neutral-400">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus a enim sit amet sapien laoreet gravida sed
+      lacinia velit. Mauris placerat nisi vel erat suscipit commodo.
+    </p>
+  );
+}
+
+export function ParagraphBodyMedium() {
+  return (
+    <p class="dark:text-white text-sm font-sans font-normal leading-[175%] tracking-normal text-neutral-400">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus a enim sit amet sapien laoreet gravida sed
+      lacinia velit. Mauris placerat nisi vel erat suscipit commodo.
+    </p>
+  );
+}
+
+export function ParagraphBodyMediumBold() {
+  return (
+    <p class="dark:text-white text-sm font-sans font-bold leading-[175%] tracking-normal text-neutral-400">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus a enim sit amet sapien laoreet gravida sed
+      lacinia velit. Mauris placerat nisi vel erat suscipit commodo.
+    </p>
+  );
+}
+
+export function ParagraphBodySmall() {
+  return (
+    <p class="dark:text-white text-xs font-sans font-normal leading-[150%] tracking-normal text-neutral-400">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus a enim sit amet sapien laoreet gravida sed
+      lacinia velit. Mauris placerat nisi vel erat suscipit commodo.
+    </p>
+  );
+}
