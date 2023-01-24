dcf28f822a44cab9fe2d80366eed95fc39d507fd
Mateusz Górny <71389330+Matgorn@users.noreply.github.com>
2023-01-24 10:41:38 +0100

    Docs' components' styles rewrite to tailwind (#20)
    chore: move components' astro and .css styles to tailwind

--- apps/docs/src/components/Header/Search.tsx
+++ apps/docs/src/components/Header/Search.tsx
@@ -1,97 +1,102 @@
 /** @jsxImportSource react */
-import { useState, useCallback, useRef } from 'react';
-import { ALGOLIA } from '../../config';
-import '@docsearch/css';
-import './Search.css';
+import { useState, useCallback, useRef } from "react";
+import { ALGOLIA } from "../../config";
+import "@docsearch/css";
 
-import { createPortal } from 'react-dom';
-import * as docSearchReact from '@docsearch/react';
+import { createPortal } from "react-dom";
+import * as docSearchReact from "@docsearch/react";
 
 /** FIXME: This is still kinda nasty, but DocSearch is not ESM ready. */
 const DocSearchModal =
-	docSearchReact.DocSearchModal || (docSearchReact as any).default.DocSearchModal;
+  docSearchReact.DocSearchModal ||
+  (docSearchReact as any).default.DocSearchModal;
 const useDocSearchKeyboardEvents =
-	docSearchReact.useDocSearchKeyboardEvents ||
-	(docSearchReact as any).default.useDocSearchKeyboardEvents;
+  docSearchReact.useDocSearchKeyboardEvents ||
+  (docSearchReact as any).default.useDocSearchKeyboardEvents;
 
 export default function Search() {
-	const [isOpen, setIsOpen] = useState(false);
-	const searchButtonRef = useRef<HTMLButtonElement>(null);
-	const [initialQuery, setInitialQuery] = useState('');
+  const [isOpen, setIsOpen] = useState(false);
+  const searchButtonRef = useRef<HTMLButtonElement>(null);
+  const [initialQuery, setInitialQuery] = useState("");
 
-	const onOpen = useCallback(() => {
-		setIsOpen(true);
-	}, [setIsOpen]);
+  const onOpen = useCallback(() => {
+    setIsOpen(true);
+  }, [setIsOpen]);
 
-	const onClose = useCallback(() => {
-		setIsOpen(false);
-	}, [setIsOpen]);
+  const onClose = useCallback(() => {
+    setIsOpen(false);
+  }, [setIsOpen]);
 
-	const onInput = useCallback(
-		(e) => {
-			setIsOpen(true);
-			setInitialQuery(e.key);
-		},
-		[setIsOpen, setInitialQuery]
-	);
+  const onInput = useCallback(
+    (e) => {
+      setIsOpen(true);
+      setInitialQuery(e.key);
+    },
+    [setIsOpen, setInitialQuery]
+  );
 
-	useDocSearchKeyboardEvents({
-		isOpen,
-		onOpen,
-		onClose,
-		onInput,
-		searchButtonRef,
-	});
+  useDocSearchKeyboardEvents({
+    isOpen,
+    onOpen,
+    onClose,
+    onInput,
+    searchButtonRef,
+  });
 
-	return (
-		<>
-			<button type="button" ref={searchButtonRef} onClick={onOpen} className="search-input">
-				<svg width="24" height="24" fill="none">
-					<path
-						d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-						stroke="currentColor"
-						strokeWidth="2"
-						strokeLinecap="round"
-						strokeLinejoin="round"
-					/>
-				</svg>
+  return (
+    <>
+      <button
+        type="button"
+        ref={searchButtonRef}
+        onClick={onOpen}
+        className="flex gap-0.5 grow box-border w-full m-0 py-[0.33em] px-[0.5em] overflow-visible font-medium text-sm font-inherit bg-theme-gray-950 dark:bg-theme-gray-100 border rounded border-theme-gray-950 dark:border-theme-gray-100 text-theme-gray-400 dark:text-theme-gray-800 outline-0 cursor-pointer transition-colors hover:text-theme-gray-100 dark:hover:text-theme-gray-900 focus-visible:text-theme-gray-100 dark:focus-visible:text-theme-gray-900 hover:border hover:border-theme-gray-400 dark:hover:border-theme-gray-800 focus-visible:border-theme-gray-400 dark:focus-visible:border-theme-gray-800 placeholder:font-medium placeholder:hover:text-theme-gray-400 placeholder:dark:hover:text-theme-gray-800 placeholder:focus-visible:text-theme-gray-400 placeholder:dark:focus-visible:text-theme-gray-800"
+      >
+        <svg width="24" height="24" fill="none">
+          <path
+            d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+        </svg>
 
-				<span>Search</span>
+        <span>Search</span>
 
-				<span className="search-hint">
-					<span className="sr-only">Press </span>
+        <span className="absolute top-[7px] right-[19px] py-[3px] px-[5px] hidden rounded md:flex items-center justify-center tracking-[0.125em] text-[13px] pointer-events-none border border-theme-gray-800 dark:border-theme-gray-400 text-theme-gray-400 dark:border-theme-gray-800 leading-[14px]">
+          <span className="sr-only">Press </span>
 
-					<kbd>/</kbd>
+          <kbd>/</kbd>
 
-					<span className="sr-only"> to search</span>
-				</span>
-			</button>
+          <span className="sr-only"> to search</span>
+        </span>
+      </button>
 
-			{isOpen &&
-				createPortal(
-					<DocSearchModal
-						initialQuery={initialQuery}
-						initialScrollY={window.scrollY}
-						onClose={onClose}
-						indexName={ALGOLIA.indexName}
-						appId={ALGOLIA.appId}
-						apiKey={ALGOLIA.apiKey}
-						transformItems={(items) => {
-							return items.map((item) => {
-								// We transform the absolute URL into a relative URL to
-								// work better on localhost, preview URLS.
-								const a = document.createElement('a');
-								a.href = item.url;
-								const hash = a.hash === '#overview' ? '' : a.hash;
-								return {
-									...item,
-									url: `${a.pathname}${hash}`,
-								};
-							});
-						}}
-					/>,
-					document.body
-				)}
-		</>
-	);
+      {isOpen &&
+        createPortal(
+          <DocSearchModal
+            initialQuery={initialQuery}
+            initialScrollY={window.scrollY}
+            onClose={onClose}
+            indexName={ALGOLIA.indexName}
+            appId={ALGOLIA.appId}
+            apiKey={ALGOLIA.apiKey}
+            transformItems={(items) => {
+              return items.map((item) => {
+                // We transform the absolute URL into a relative URL to
+                // work better on localhost, preview URLS.
+                const a = document.createElement("a");
+                a.href = item.url;
+                const hash = a.hash === "#overview" ? "" : a.hash;
+                return {
+                  ...item,
+                  url: `${a.pathname}${hash}`,
+                };
+              });
+            }}
+          />,
+          document.body
+        )}
+    </>
+  );
 }
--- packages/selleo-tailwind/tailwind.config.cjs
+++ packages/selleo-tailwind/tailwind.config.cjs
@@ -1,23 +1,23 @@
 /** @type {import('tailwindcss').Config} */
-const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-  darkMode: 'class',
+  darkMode: "class",
   theme: {
     fontFamily: {
-      sans: ['sofia-pro', ...defaultTheme.fontFamily.sans],
-      'sofia-pro-soft': ['sofia-pro-soft', ...defaultTheme.fontFamily.sans],
+      sans: ["sofia-pro", ...defaultTheme.fontFamily.sans],
+      "sofia-pro-soft": ["sofia-pro-soft", ...defaultTheme.fontFamily.sans],
     },
     fontSize: {
-      xs: '14px',
-      sm: '16px',
-      base: '18px',
-      md: '24px',
-      lg: '32px',
-      xl: '40px',
-      xxl: '56px',
-      xxxl: '72px'
+      xs: "14px",
+      sm: "16px",
+      base: "18px",
+      md: "24px",
+      lg: "32px",
+      xl: "40px",
+      xxl: "56px",
+      xxxl: "72px",
     },
     spacing: {
       0: 0,
@@ -32,46 +32,74 @@ module.exports = {
       8: "144px",
       9: "192px",
     },
+    screens: {
+      sm: "576px",
+      md: "768px",
+      lg: "992px",
+      xl: "1200px",
+      touch: {
+        raw: "not ((any-hover: hover) or (any-pointer: fine))",
+      },
+    },
     extend: {
+      borderWidth: {
+        3: "3px",
+      },
       colors: {
         brand: {
           primary: {
-            100: '#fff8f5',
-            200: '#ffeae0',
-            400: '#ff905d',
-            500: '#ff6d2a', //DEFAULT
-            600: '#e25718',
+            100: "#fff8f5",
+            200: "#ffeae0",
+            400: "#ff905d",
+            500: "#ff6d2a", //DEFAULT
+            600: "#e25718",
           },
           secondary: {
-            100: '#f3f7fc',
-            200: '#5ac0f6',
-            500: '#2aaef4', //DEFAULT
-            600: '#3871a5'
+            100: "#f3f7fc",
+            200: "#5ac0f6",
+            500: "#2aaef4", //DEFAULT
+            600: "#3871a5",
           },
         },
-        white: '#ffffff',
-        black: '#2F3033',
-        danger: '#E81B2F',
+        white: "#ffffff",
+        black: "#2F3033",
+        danger: "#E81B2F",
         neutral: {
-          100: '#ff6d2a',
-          200: '#eaeaeb',
-          300: '#bfc0c4',
-          400: '#95979d',
-          500: '#696b72',
-          600: '#4c4d52',
+          100: "#f4f5f5",
+          200: "#eaeaeb",
+          300: "#bfc0c4",
+          400: "#95979d",
+          500: "#696b72",
+          600: "#4c4d52",
+        },
+        theme: {
+          dark: "#1f2937",
+          accent: "#3895ff",
+          gray: {
+            100: "#16191d",
+            400: "#586474",
+            800: "#c5cbd3",
+            900: "#e2e5e9",
+            950: "#f0f2f4",
+          },
         },
       },
       letterSpacing: {
-        normal: '0.01em',
-        wide: '0.15em'
+        normal: "0.01em",
+        wide: "0.15em",
       },
       lineHeight: {
-        tight: '21px',
-        relaxed: '1.75'
+        tight: "21px",
+        relaxed: "1.75",
       },
       fontWeight: {
-        black: 800
-      }
+        black: 800,
+      },
+      screens: {
+        touch: {
+          raw: "not ((any-hover: hover) or (any-pointer: fine))",
+        },
+      },
     },
   },
   plugins: [],
