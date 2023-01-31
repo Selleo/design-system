fb288d579ee7ebe4df8153b31104e6cac7bb4876
matgorn <m.gorny@selleo.com>
2023-01-31 16:47:02 +0100

    chore: encapsulate icons to individual components

---
+++ packages/selleo-design-core/src/icons/BookingDeskIcon.tsx
@@ -0,0 +1,67 @@
+import { h } from "preact";
+
+export function BookingDeskIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+    >
+      <path
+        d="M3.99667 10.9996V21.0037"
+        stroke="currentColor"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M9.99919 2.99625H14.0009C15.1059 2.99625 16.0017 3.89205 16.0017 4.99708V10.9996H7.99835V4.99708C7.99835 3.89205 8.89416 2.99625 9.99919 2.99625Z"
+        stroke="currentColor"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M7.99834 14.0008H16.0017C16.5542 14.0008 17.0021 14.4487 17.0021 15.0012V15.5014C17.0021 16.3302 16.3302 17.0021 15.5015 17.0021H8.49855C7.66978 17.0021 6.99792 16.3302 6.99792 15.5014V15.0012C6.99792 14.4487 7.44583 14.0008 7.99834 14.0008Z"
+        stroke="currentColor"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M12 17.0021V21.0037"
+        stroke="currentColor"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M15.0012 21.0037H8.99872"
+        stroke="currentColor"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M22.0042 10.9996H1.99585"
+        stroke="currentColor"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M20.0033 21.0037V10.9996"
+        stroke="currentColor"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+}
---
+++ packages/selleo-design-core/src/icons/UsersIcon.tsx
@@ -0,0 +1,41 @@
+import { h } from "preact";
+
+export function UsersIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+    >
+      <circle
+        cx="12"
+        cy="12"
+        r="9.00375"
+        stroke="currentColor"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M15.6125 16.5019H8.38747C8.0805 16.5013 7.83179 16.2526 7.83124 15.9456V15.4944C7.83509 14.2105 8.87526 13.1708 10.1592 13.1675H13.8407C15.1247 13.1708 16.1649 14.2105 16.1687 15.4944V15.9456C16.1682 16.2526 15.9194 16.5013 15.6125 16.5019Z"
+        stroke="currentColor"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M12 10.9996C10.758 10.9996 9.75074 9.99361 9.74909 8.75163C9.74744 7.50965 10.752 6.50102 11.994 6.4977C13.236 6.4944 14.246 7.49766 14.251 8.73964C14.2528 9.33802 14.0165 9.91256 13.5943 10.3365C13.172 10.7605 12.5984 10.9991 12 10.9996V10.9996Z"
+        stroke="currentColor"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+}
