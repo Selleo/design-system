44e86950202f02b87465d49db69b5d81fd78e147
matgorn <m.gorny@selleo.com>
2023-02-02 12:03:20 +0100

    feat: add new variants with icons to the button preview page

---
+++ packages/selleo-design-core/src/icons/PlaceholderIcon.tsx
@@ -0,0 +1,27 @@
+import { h } from "preact";
+
+export function PlaceholderIcon(props: h.JSX.SVGAttributes<SVGSVGElement>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 25 24"
+      fill="none"
+      {...props}
+    >
+      <path
+        d="M12.5 8V16"
+        stroke="currentColor"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M16.5 12H8.5"
+        stroke="currentColor"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+}
--- packages/selleo-design-core/src/icons/index.ts
+++ packages/selleo-design-core/src/icons/index.ts
@@ -1,1 +1,2 @@
+export * from "./PlaceholderIcon";
 export * from "./CheckIcon";
