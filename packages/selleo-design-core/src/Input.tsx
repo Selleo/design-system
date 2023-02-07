a7cc432682535e02f438301715555d4705fbeff1
matgorn <m.gorny@selleo.com>
2023-02-07 13:06:19 +0100

    chore: input component refactor and styles update

--- packages/selleo-design-core/src/Input.tsx
+++ packages/selleo-design-core/src/Input.tsx
@@ -1,243 +1,88 @@
 import { h } from "preact";
+import cx from "classnames";
 
-export function Input() {
-  return (
-    <label class="relative block">
-      <input
-        class="w-full py-2 px-[48px] bg-white text-neutral-600 hover:placeholder:text-neutral-600 focus:placeholder:text-neutral-600 border-2 rounded-lg border-neutral-200 outline-brand-primary-500 hover:border-neutral-300 peer disabled:bg-neutral-200 disabled:border-neutral-400 disabled:placeholder:text-neutral-500 invalid:border-danger"
-        placeholder="Input placeholder"
-      />
-      <span class="absolute inset-y-0 left-0 flex items-center pl-2 stroke-neutral-300 peer-hover:stroke-neutral-600 peer-focus:stroke-neutral-600 peer-[&:not(:placeholder-shown)]:stroke-neutral-600 peer-disabled:stroke-neutral-500">
-        <svg
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M20.0034 20.0033L16.7521 16.752"
-            class="stroke-inherit"
-            stroke-width="1.5"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          />
-          <path
-            d="M16.0962 6.90377C18.6346 9.44214 18.6346 13.5578 16.0962 16.0962C13.5579 18.6346 9.44215 18.6346 6.90378 16.0962C4.36541 13.5578 4.36541 9.44214 6.90378 6.90377C9.44215 4.3654 13.5579 4.3654 16.0962 6.90377"
-            class="stroke-inherit"
-            stroke-width="1.5"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          />
-        </svg>
-      </span>
+type Props = {
+  size: "small" | "normal";
+  variant: "error" | "default";
+  iconStart?(props: h.JSX.SVGAttributes<SVGSVGElement>): h.JSX.Element;
+  iconEnd?(props: h.JSX.SVGAttributes<SVGSVGElement>): h.JSX.Element;
+} & Omit<h.JSX.HTMLAttributes<HTMLInputElement>, "size">;
 
-      <span class="absolute inset-y-0 right-0 flex items-center pr-2 stroke-neutral-300 peer-hover:stroke-neutral-600 peer-focus:stroke-neutral-600 peer-[&:not(:placeholder-shown)]:stroke-neutral-600 peer-disabled:stroke-neutral-500">
-        <svg
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M20.0034 20.0033L16.7521 16.752"
-            class="stroke-inherit"
-            stroke-width="1.5"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          />
-          <path
-            d="M16.0962 6.90377C18.6346 9.44214 18.6346 13.5578 16.0962 16.0962C13.5579 18.6346 9.44215 18.6346 6.90378 16.0962C4.36541 13.5578 4.36541 9.44214 6.90378 6.90377C9.44215 4.3654 13.5579 4.3654 16.0962 6.90377"
-            class="stroke-inherit"
-            stroke-width="1.5"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          />
-        </svg>
-      </span>
-    </label>
+export function Input({
+  size = "normal",
+  variant = "default",
+  iconStart,
+  iconEnd,
+  placeholder = "Input placeholder",
+  label,
+  disabled,
+  ...props
+}: Props) {
+  const IconStart = iconStart;
+  const IconEnd = iconEnd;
+
+  const iconSize = size === "normal" ? 24 : 16;
+
+  const wrapperClasses = cx({
+    "opacity-50": disabled,
+  });
+
+  const inputWrapperClasses = cx(
+    "group relative flex rounded border outline-0 text-neutral-300 hover:text-neutral-600 focus-within:text-neutral-600 focus-within:border-brand-primary-500 hover:focus-within:border-brand-primary-500 disabled:text-danger",
+    {
+      "bg-white border-neutral-200 hover:border-neutral-300":
+        !disabled && variant !== "error",
+      "bg-neutral-200 border-neutral-500 hover:border-neutral-500": disabled,
+      "border-danger hover:border-danger": variant === "error",
+      "pl-2": !IconStart,
+    }
   );
-}
 
-export function DisabledInput() {
-  return (
-    <label class="relative block">
-      <input
-        class="w-full py-2 px-[48px] bg-white text-neutral-600 hover:placeholder:text-neutral-600 focus:placeholder:text-neutral-600 border-2 rounded-lg border-neutral-200 outline-brand-primary-500 hover:border-neutral-300 peer disabled:bg-neutral-200 disabled:border-neutral-400 disabled:placeholder:text-neutral-500 invalid:border-danger"
-        placeholder="Input placeholder"
-        disabled
-      />
-      <span class="absolute inset-y-0 left-0 flex items-center pl-2 stroke-neutral-300 peer-hover:stroke-neutral-600 peer-focus:stroke-neutral-600 peer-[&:not(:placeholder-shown)]:stroke-neutral-600 peer-disabled:stroke-neutral-500">
-        <svg
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M20.0034 20.0033L16.7521 16.752"
-            class="stroke-inherit"
-            stroke-width="1.5"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          />
-          <path
-            d="M16.0962 6.90377C18.6346 9.44214 18.6346 13.5578 16.0962 16.0962C13.5579 18.6346 9.44215 18.6346 6.90378 16.0962C4.36541 13.5578 4.36541 9.44214 6.90378 6.90377C9.44215 4.3654 13.5579 4.3654 16.0962 6.90377"
-            class="stroke-inherit"
-            stroke-width="1.5"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          />
-        </svg>
-      </span>
+  const iconClasses =
+    "flex items-center peer-valid:text-neutral-600 peer-disabled:text-neutral-500";
 
-      <span class="absolute inset-y-0 right-0 flex items-center pr-2 stroke-neutral-300 peer-hover:stroke-neutral-600 peer-focus:stroke-neutral-600 peer-[&:not(:placeholder-shown)]:stroke-neutral-600 peer-disabled:stroke-neutral-500">
-        <svg
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M20.0034 20.0033L16.7521 16.752"
-            class="stroke-inherit"
-            stroke-width="1.5"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          />
-          <path
-            d="M16.0962 6.90377C18.6346 9.44214 18.6346 13.5578 16.0962 16.0962C13.5579 18.6346 9.44215 18.6346 6.90378 16.0962C4.36541 13.5578 4.36541 9.44214 6.90378 6.90377C9.44215 4.3654 13.5579 4.3654 16.0962 6.90377"
-            class="stroke-inherit"
-            stroke-width="1.5"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          />
-        </svg>
-      </span>
-    </label>
+  const startIconClasses = cx(iconClasses, "order-first", {
+    "pl-1": size === "small",
+    "pl-2": size === "normal",
+  });
+
+  const endIconClasses = cx(iconClasses, {
+    "pr-1": size === "small",
+    "pr-2": size === "normal",
+  });
+
+  const inputClasses = cx(
+    "peer rounded w-full pl-1 outline-0 placeholder:text-neutral-300 group-hover:placeholder:text-neutral-600 focus:placeholder:text-neutral-600 valid:text-neutral-600 disabled:bg-neutral-200 disabled:placeholder:text-neutral-500",
+    {
+      "py-2 text-sm": size === "normal",
+      "py-1 text-xs": size === "small",
+    }
   );
-}
 
-export function ErrorInput() {
   return (
-    <label class="relative block">
-      <input
-        class="w-full py-2 px-[48px] bg-white text-neutral-600 hover:placeholder:text-neutral-600 focus:placeholder:text-neutral-600 border-2 rounded-lg border-neutral-200 outline-brand-primary-500 hover:border-neutral-300 peer disabled:bg-neutral-200 disabled:border-neutral-400 disabled:placeholder:text-neutral-500 invalid:border-danger"
-        placeholder="Input placeholder"
-        required
-      />
-      <span class="absolute inset-y-0 left-0 flex items-center pl-2 stroke-neutral-300 peer-hover:stroke-neutral-600 peer-focus:stroke-neutral-600 peer-[&:not(:placeholder-shown)]:stroke-neutral-600 peer-disabled:stroke-neutral-500">
-        <svg
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M20.0034 20.0033L16.7521 16.752"
-            class="stroke-inherit"
-            stroke-width="1.5"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          />
-          <path
-            d="M16.0962 6.90377C18.6346 9.44214 18.6346 13.5578 16.0962 16.0962C13.5579 18.6346 9.44215 18.6346 6.90378 16.0962C4.36541 13.5578 4.36541 9.44214 6.90378 6.90377C9.44215 4.3654 13.5579 4.3654 16.0962 6.90377"
-            class="stroke-inherit"
-            stroke-width="1.5"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          />
-        </svg>
-      </span>
-
-      <span class="absolute inset-y-0 right-0 flex items-center pr-2 stroke-neutral-300 peer-hover:stroke-neutral-600 peer-focus:stroke-neutral-600 peer-[&:not(:placeholder-shown)]:stroke-neutral-600 peer-disabled:stroke-neutral-500">
-        <svg
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M20.0034 20.0033L16.7521 16.752"
-            class="stroke-inherit"
-            stroke-width="1.5"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          />
-          <path
-            d="M16.0962 6.90377C18.6346 9.44214 18.6346 13.5578 16.0962 16.0962C13.5579 18.6346 9.44215 18.6346 6.90378 16.0962C4.36541 13.5578 4.36541 9.44214 6.90378 6.90377C9.44215 4.3654 13.5579 4.3654 16.0962 6.90377"
-            class="stroke-inherit"
-            stroke-width="1.5"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          />
-        </svg>
-      </span>
-    </label>
-  );
-}
-
-export function SmallInput() {
-  return (
-    <label class="relative block">
-      <input
-        class="w-full py-1 px-4 bg-white text-neutral-600 hover:placeholder:text-neutral-600 focus:placeholder:text-neutral-600 border-2 rounded-lg border-neutral-200 outline-brand-primary-500 hover:border-neutral-300 peer disabled:bg-neutral-200 disabled:border-neutral-400 disabled:placeholder:text-neutral-500 invalid:border-danger text-xs"
-        placeholder="Input placeholder"
-      />
-      <span class="absolute inset-y-0 left-0 flex items-center pl-1 stroke-neutral-300 peer-hover:stroke-neutral-600 peer-focus:stroke-neutral-600 peer-[&:not(:placeholder-shown)]:stroke-neutral-600 peer-disabled:stroke-neutral-500">
-        <svg
-          width="16"
-          height="16"
-          viewBox="0 0 16 16"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M9.46217 3.72125C11.0167 5.27579 11.0167 7.79621 9.46217 9.35076C7.90762 10.9053 5.3872 10.9053 3.83266 9.35076C2.27811 7.79621 2.27811 5.27579 3.83266 3.72125C5.3872 2.1667 7.90762 2.1667 9.46217 3.72125"
-            class="stroke-inherit"
-            stroke-width="1.5"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          />
-          <path
-            d="M9.43335 9.37334L13.3333 13.3267"
-            class="stroke-inherit"
-            stroke-width="1.5"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          />
-        </svg>
-      </span>
-
-      <span class="absolute inset-y-0 right-0 flex items-center pr-1 stroke-neutral-300 peer-hover:stroke-neutral-600 peer-focus:stroke-neutral-600 peer-[&:not(:placeholder-shown)]:stroke-neutral-600 peer-disabled:stroke-neutral-500">
-        <svg
-          width="16"
-          height="16"
-          viewBox="0 0 16 16"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M9.46217 3.72125C11.0167 5.27579 11.0167 7.79621 9.46217 9.35076C7.90762 10.9053 5.3872 10.9053 3.83266 9.35076C2.27811 7.79621 2.27811 5.27579 3.83266 3.72125C5.3872 2.1667 7.90762 2.1667 9.46217 3.72125"
-            class="stroke-inherit"
-            stroke-width="1.5"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          />
-          <path
-            d="M9.43335 9.37334L13.3333 13.3267"
-            class="stroke-inherit"
-            stroke-width="1.5"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          />
-        </svg>
-      </span>
-    </label>
+    <div class={wrapperClasses}>
+      {label && (
+        <label class="block text-xs text-neutral-500 mb-1">{label}</label>
+      )}
+      <div class={inputWrapperClasses}>
+        <input
+          class={inputClasses}
+          placeholder={placeholder}
+          required
+          disabled={disabled}
+          {...props}
+        />
+        {IconStart && (
+          <span class={startIconClasses}>
+            <IconStart width={iconSize} height={iconSize} />
+          </span>
+        )}
+        {IconEnd && (
+          <span class={endIconClasses}>
+            <IconEnd width={iconSize} height={iconSize} />
+          </span>
+        )}
+      </div>
+    </div>
   );
 }
---
+++ packages/selleo-design-core/src/icons/SearchIcon.tsx
@@ -0,0 +1,27 @@
+import { h } from "preact";
+
+export function SearchIcon(props: h.JSX.SVGAttributes<SVGSVGElement>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="none"
+      {...props}
+    >
+      <path
+        d="M14.1936 5.5809C16.5254 7.91272 16.5254 11.6933 14.1936 14.0252C11.8618 16.357 8.08117 16.357 5.74935 14.0252C3.41753 11.6933 3.41753 7.91272 5.74935 5.5809C8.08117 3.24908 11.8618 3.24908 14.1936 5.5809"
+        stroke="currentColor"
+        stroke-width="2.25"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M14.1504 14.0596L20.0004 19.9896"
+        stroke="currentColor"
+        stroke-width="2.25"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+}
