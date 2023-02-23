67453fee92475162bc2b99af921b679afc516722
Dawid Haratyk <dharatyk@onet.pl>
2023-02-23 14:51:24 +0100

    feat: add Avatar and Testimonials components

---
+++ packages/selleo-design-core/src/Avatars.tsx
@@ -0,0 +1,73 @@
+import { h } from 'preact';
+import cx from 'classnames';
+
+export type AvatarProps = {
+  hideName?: boolean;
+  size: 'regular' | 'big';
+  image?: h.JSX.Element | null;
+  username?: string;
+  imageClass?: string;
+  containerClass?: string;
+};
+
+export function Avatar({
+  hideName = false,
+  size,
+  image,
+  username,
+  imageClass,
+  containerClass,
+}: AvatarProps) {
+  const imageClasses = cx(
+    `rounded-full overflow-hidden bg-white flex justify-center items-center`,
+    {
+      'mr-1': !hideName,
+      'w-[24px] h-[24px]': size === 'regular',
+      'w-[32px] h-[32px]': size === 'big',
+    },
+    imageClass
+  );
+
+  const containerClasses = cx(
+    `flex flex-row items-center min-w-fit h-auto`,
+    {
+      'h-[24px]': size === 'regular',
+      'h-[32px]': size === 'big',
+      'h-auto': !image,
+    },
+    containerClass
+  );
+
+  const usernameClasses = cx(
+    'text-xxs font-normal text-neutral-600 dark:text-white',
+    {
+      'text-xs': size === 'big',
+    }
+  );
+
+  const nameInitials = username?.split(' ').map((word) => word[0]);
+
+  const nameInitialsClasses = cx('text-neutral-600', {
+    'text-xs': size === 'big',
+    'text-xxxs': size === 'regular',
+  });
+
+  const imageOrNameInitials = (
+    <div className={imageClasses}>
+      {image ??
+        (nameInitials && (
+          <span className={nameInitialsClasses}>
+            {nameInitials[0]}
+            {nameInitials[1]}
+          </span>
+        ))}
+    </div>
+  );
+
+  return (
+    <div className={containerClasses}>
+      {imageOrNameInitials}
+      {!hideName && <h4 className={usernameClasses}>{username}</h4>}
+    </div>
+  );
+}
--- packages/selleo-design-core/src/Card.tsx
+++ packages/selleo-design-core/src/Card.tsx
@@ -8,8 +8,9 @@ type CardProps = {
 
 export const Card = ({ children, className }: CardProps) => {
   const classes = cx(
-    `py-3 flex justify-center items-center bg-brand-secondary-100 ${className}`
+    'py-3 flex justify-center items-center bg-brand-secondary-100',
+    className
   );
 
-  return <div class={classes}>{children}</div>;
+  return <div className={classes}>{children}</div>;
 };
