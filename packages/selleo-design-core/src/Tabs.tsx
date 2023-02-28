9e35f64bd83146298a994773e7cd5b8780ab0b4e
Dawid Haratyk <dawidharatyk@onet.pl>
2023-02-28 13:53:40 +0100

    refactor: change colors to design-system palette and add list of tabs

--- packages/selleo-design-core/src/Tabs.tsx
+++ packages/selleo-design-core/src/Tabs.tsx
@@ -1,25 +1,45 @@
-import { h } from "preact";
-import cx from "classnames";
+import { h } from 'preact';
+import cx from 'classnames';
 
 type TabProps = {
   active: boolean;
-  icon?: JSX.Element;
+  icon?(): h.JSX.Element;
 };
 
+type TabsProps = {
+  tabsAmount: number;
+  selectedId: number;
+};
+
+export function Tabs({ tabsAmount, selectedId }: TabsProps) {
+  const tabs = Array.from({ length: tabsAmount }, (_, key) => (
+    <Tab active={selectedId === key + 1} />
+  ));
+
+  return <div class="space-x-1">{tabs}</div>;
+}
+
 export function Tab({ active, icon }: TabProps) {
+  const Icon = icon;
+
   const classes = cx(
-    "px-2 py-1 text-base font-normal rounded-lg cursor-pointer",
+    'px-2 py-1 text-base font-normal rounded-lg cursor-pointer',
     {
-      "text-[#FF6D2A] bg-[#FFF8F5]": active,
-      "text-[#4C4D52] bg-white hover:bg-[#FFF8F5] dark:text-white dark:bg-[#2F3033] dark:hover:bg-[#4C4D52]":
+      'text-brand-primary-500 bg-brand-primary-100': active,
+      'text-neutral-600 bg-transparent hover:bg-brand-primary-100 dark:text-white dark:bg-transparent dark:hover:bg-neutral-600':
         !active,
+      'inline-flex items-center': Icon,
     }
   );
 
+  const textClasses = cx({
+    'ml-1': Icon,
+  });
+
   return (
     <span class={classes}>
-      {icon && "x "}
-      Item
+      {Icon && <Icon />}
+      <span class={textClasses}>Item</span>
     </span>
   );
 }
