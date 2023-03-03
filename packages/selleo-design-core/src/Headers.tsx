a1d59d5bfbf51be630901816b47a821918e703b6
Dawid Haratyk <dawidharatyk@onet.pl>
2023-03-03 12:09:54 +0100

    feat: arrange the sidebar, add typography page

--- packages/selleo-design-core/src/Headers.tsx
+++ packages/selleo-design-core/src/Headers.tsx
@@ -1,44 +1,48 @@
-import { h } from "preact";
+import { ComponentChildren, h } from 'preact';
 
-export function H1() {
+type Header = {
+  children: ComponentChildren;
+};
+
+export function H1({ children }: Header) {
   return (
     <h1 class="font-sans font-normal text-xxl leading-125 tracking-none">
-      H1 Header
+      {children}
     </h1>
   );
 }
-export function H2() {
+export function H2({ children }: Header) {
   return (
     <h2 class="font-sans font-bold text-xl leading-125 tracking-normal">
-      H2 Header
+      {children}
     </h2>
   );
 }
-export function H3() {
+export function H3({ children }: Header) {
   return (
     <h3 class="font-sans font-bold text-lg leading-125 tracking-normal">
-      H3 Header
+      {children}
     </h3>
   );
 }
-export function H4() {
+export function H4({ children }: Header) {
   return (
     <h4 class="font-sans font-bold text-md leading-125 tracking-normal">
-      H4 Header
+      {children}
     </h4>
   );
 }
-export function H5() {
+export function H5({ children }: Header) {
   return (
     <h5 class="font-sans font-bold text-sm leading-150 tracking-normal">
-      H5 Header
+      {children}
     </h5>
   );
 }
-export function H6() {
+export function H6({ children }: Header) {
   return (
     <h6 class="font-sans font-bold text-xs leading-tight tracking-normal">
-      H6 Header
+      {children}
     </h6>
   );
 }
