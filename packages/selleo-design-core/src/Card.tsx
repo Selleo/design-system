d64b03c0e08b858c495904022d198d83e201ee32
Dawid Haratyk <dawidharatyk@onet.pl>
2023-03-06 08:43:01 +0100

    feat: possibility to add additional styles to Card

--- packages/selleo-design-core/src/Card.tsx
+++ packages/selleo-design-core/src/Card.tsx
@@ -1,13 +1,15 @@
 import { h } from 'preact';
+import cx from 'classnames';
 
 type CardProps = {
   children: h.JSX.Element;
+  className?: string;
 };
 
-export const Card = ({ children }: CardProps) => {
-  return (
-    <div class="py-3 flex justify-center items-center bg-brand-secondary-100">
-      {children}
-    </div>
+export const Card = ({ children, className }: CardProps) => {
+  const classes = cx(
+    `py-3 flex justify-center items-center bg-brand-secondary-100 ${className}`
   );
+
+  return <div class={classes}>{children}</div>;
 };
