122c6a0aed6f4f63c3e67d60e06c7cf2f7be75c6
Dawid Haratyk <dawidharatyk@onet.pl>
2023-03-06 09:51:52 +0100

    refactor: remove unneccessary tags and add styles to a Card

--- packages/selleo-design-core/src/Card.tsx
+++ packages/selleo-design-core/src/Card.tsx
@@ -8,7 +8,8 @@ type CardProps = {
 
 export const Card = ({ children, className }: CardProps) => {
   const classes = cx(
-    `py-3 flex justify-center items-center bg-brand-secondary-100 ${className}`
+    'py-3 flex justify-center items-center bg-brand-secondary-100',
+    className
   );
 
   return <div class={classes}>{children}</div>;
