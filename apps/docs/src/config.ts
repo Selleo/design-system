4fc307e6c59cff0b95d039081b34618e9049fa2b
Dawid Haratyk <dawidharatyk@onet.pl>
2023-03-11 13:18:33 +0100

    feat: add logos page to a brand section

--- apps/docs/src/config.ts
+++ apps/docs/src/config.ts
@@ -46,6 +46,7 @@ export type Sidebar = Record<
 
 export const SIDEBAR: Sidebar = {
   en: {
+    Brand: [{ text: 'Logos', link: 'brand-logos' }],
     Components: [
       { text: 'App Header', link: 'app-header' },
       { text: 'Avatars', link: 'avatars' },
